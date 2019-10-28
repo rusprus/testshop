1c9a5dbba0c191e9dada415cbcbb3214d5ad3dba
Ruslan Yakupov <kazanpiter@gmail.com>
2019-10-28 13:12:37 +0300

    Создание моделей заказов и элементов заказов. Связывание этих моделей. Создание CRUD-апросов для модели заказов.

---
+++ models/Order.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\db\Expression;
+use app\models\OrderItems;
+
+/**
+ * This is the model class for table "order".
+ *
+ * @property int $id
+ * @property string $created_at
+ * @property string $updated_at
+ * @property double $sum
+ * @property string $status
+ * @property string $name
+ * @property string $phone
+ */
+class Order extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'order';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['created_at', 'updated_at'], 'safe'],
+            [['sum', 'phone'], 'required'],
+            [['sum'], 'number'],
+            [['status'], 'string'],
+            [['name', 'phone'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'created_at' => 'Время создания',
+            'updated_at' => 'Время обновления',
+            'sum' => 'Сумма',
+            'status' => 'Статус',
+            'phone' => 'Телефон',
+            'name' => 'Имя',
+        ];
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+                // если вместо метки времени UNIX используется datetime:
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrderItems()
+    {
+        return $this->hasMany(OrderItems::className(), ['order_id'=>'id']);
+    }
+}
