b3670493d4dd82c62519d865e8ec8d0e0e3d64f3
Ruslan Yakupov <kazanpiter@gmail.com>
2019-10-28 12:57:08 +0300

    Создание модели продуктов. Создание CRUD-запросов к модели продуктов.

---
+++ models/Product.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "product".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $content
+ * @property double $price
+ */
+class Product extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'product';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['content'], 'string'],
+            [['price'], 'number'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'content' => 'Content',
+            'price' => 'Price',
+        ];
+    }
+}
